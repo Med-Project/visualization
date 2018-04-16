60b8bf612c42de0dff0c6ebed816da5765779ccf
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2018-04-16 19:30:10 +0600

    3D

--- app.py
+++ app.py
@@ -11,15 +11,28 @@ server = app.server
 CORS(server)
 
 df = pd.read_csv('vis_data.csv')
+df_3d = pd.read_csv('vis_data_3d.csv')
 
 unique_classes = df['categ'].unique()
 dis_dropdown_options = [{'label': str(i), 'value': str(i)} for i in unique_classes]
+
+tabs = [{'label': '2D Visualization', 'value': 1}, {'label': '3D Visualization', 'value': 2}]
 
 app.layout = html.Div([
     html.H1(
         children='Diseases Vis. Map',
         style={'textAlign': 'center'}
     ),
+
+    html.Div([
+        dcc.Tabs(
+            id='tabs',
+            tabs=tabs,
+            value=1
+        )
+    ], style={
+        'fontFamily': 'Sans-Serif'
+    }),
 
     html.Div(
         children='''
@@ -42,17 +55,19 @@ app.layout = html.Div([
 
 @app.callback(
     dash.dependencies.Output('dis-graph', 'figure'),
-    [dash.dependencies.Input('dis-dropdown', 'value')])
-def update_figure(dis_dropdown_values):
-    dff = df[df['categ'].isin(dis_dropdown_values)]
-    figure={
-        'data': [
+    [dash.dependencies.Input('dis-dropdown', 'value'),
+    dash.dependencies.Input('tabs', 'value')])
+def update_figure(dis_dropdown_values, tabs_value):
+    data = []
+    if(tabs_value == 1):
+        dff = df[df['categ'].isin(dis_dropdown_values)]
+        data = [
             go.Scatter(
                 x=dff['X'],
                 y=dff['Y'],
                 text=dff['name'] + '<br>' + dff['categ'],
                 mode='markers',
-                opacity=0.7,
+                opacity=0.8,
                 marker={
                     'size': 12,
                     'line': {'width': 0.5, 'color': 'white'},
@@ -60,15 +75,31 @@ def update_figure(dis_dropdown_values):
                 },
                 hoverinfo='text'
             )
-        ],
-        'layout': go.Layout(
-            title='Diseases map',
-            margin={'l': 40, 'b': 40, 't': 100, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest'
-        )
-    }
-    return figure
+        ]
+    else:
+        dff = df_3d[df_3d['categ'].isin(dis_dropdown_values)]
+        data = [
+            go.Scatter3d(
+                x=dff['X'],
+                y=dff['Y'],
+                z=dff['Z'],
+                text=dff['name'] + '<br>' + dff['categ'],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'color' : dff['color'],
+                },
+                hoverinfo='text'
+            )
+        ]
+    layout = go.Layout(
+        title='Diseases map',
+        height=700,
+        margin={'l': 40, 'b': 40, 't': 100, 'r': 10},
+        legend={'x': 0, 'y': 1},
+        hovermode='closest'
+    )
+    return {'data': data, 'layout': layout}
 
 if __name__ == '__main__':
-    app.run_server(port=80)
+    app.run_server()
